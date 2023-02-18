6cde9b4a70813d088dd681b03984da5638dc8d39
Ammar <ammarraneez@gmail.com>
2023-02-18 20:12:51 +0530

    Add timer, logic and final touches

--- CardWorkoutSB/CardWorkoutSB/CardSelectionViewController.swift
+++ CardWorkoutSB/CardWorkoutSB/CardSelectionViewController.swift
@@ -10,22 +10,39 @@ import UIKit
 class CardSelectionViewController: UIViewController {
     @IBOutlet var cardImageView: UIImageView!
     @IBOutlet var buttons: [UIButton]!
+    var cards: [UIImage] = Card.allValues
+    
+    var timer: Timer!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.startTimer()
         for button in self.buttons {
             button.layer.cornerRadius = 8
         }
     }
+    
+    // Once view dissappears
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        self.timer.invalidate()
+    }
+    
+    func startTimer() {
+        self.timer = Timer.scheduledTimer(timeInterval: 0.1, target: self, selector: #selector(self.showRandomImage), userInfo: nil, repeats: true)
+    }
+    
+    @objc func showRandomImage() {
+        self.cardImageView.image = self.cards.randomElement() ?? UIImage(named: "AS")
+    }
 
     @IBAction func stopButtontapped(_ sender: UIButton) {
+        timer.invalidate()
     }
     
     
     @IBAction func restartButtonTapped(_ sender: UIButton) {
-    }
-    
-    
-    @IBAction func rulesButtonTapped(_ sender: UIButton) {
+        timer.invalidate()
+        self.startTimer()
     }
 }
