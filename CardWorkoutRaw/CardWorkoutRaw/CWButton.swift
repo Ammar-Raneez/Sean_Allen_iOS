4b9f762ffa0b177ced13ef8c8858f12e9f13b606
Ammar <ammarraneez@gmail.com>
2023-02-18 21:21:22 +0530

    Create home layout

--- CardWorkoutRaw/CardWorkoutRaw/CWButton.swift
+++ CardWorkoutRaw/CardWorkoutRaw/CWButton.swift
@@ -8,6 +8,30 @@
 import UIKit
 
 class CWButton: UIButton {
-
-
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.configure()
+    }
+    
+    // Every subclass will require this
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    init(backgroundColor: UIColor, title: String) {
+        // Lay using constraints without frames
+        super.init(frame: .zero)
+        self.backgroundColor = backgroundColor
+        setTitle(title, for: .normal)
+        configure()
+    }
+    
+    func configure() {
+        layer.cornerRadius = 8
+        titleLabel?.font = .systemFont(ofSize: 19, weight: .bold)
+        setTitleColor(.white, for: .normal)
+        
+        // Use auto layout
+        translatesAutoresizingMaskIntoConstraints = false
+    }
 }
