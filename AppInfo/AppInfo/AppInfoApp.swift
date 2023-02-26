e4635f471a84a5f80a687550ae57c1accc64db25
Ammar <ammarraneez@gmail.com>
2023-02-26 16:51:28 +0530

    init project

---
+++ AppInfo/AppInfo/AppInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppInfoApp.swift
+//  AppInfo
+//
+//  Created by Ammar on 2023-02-26.
+//
+
+import SwiftUI
+
+@main
+struct AppInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
